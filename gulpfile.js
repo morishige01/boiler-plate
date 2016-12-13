1e488ff2fb1ff7ff27e2b63ac95f3c448d66d4db
YUKI NAKAMURA <nayu.fizzbot@gmail.com>
2016-12-13 14:01:47 +0900

    gulpセッティング

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
-const gulp = require('gulp');
-const ejs  = require('gulp-ejs');
+// plugin
+const gulp         = require('gulp');
+const ejs          = require('gulp-ejs');
+const sass         = require('gulp-sass');
+const plumber      = require('gulp-plumber');
+const notify       = require('gulp-notify');
+const autoprefixer = require('gulp-autoprefixer');
 
+// ejs compile
 gulp.task('ejs',()=>{
-  return gulp.src("./src/*.ejs")
+  return gulp.src("./src/ejs/*.ejs")
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(ejs('', {"ext": ".html"}))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('watch', function(){
+// sass compile
+gulp.task('sass',()=>{
+  return gulp.src("./src/scss/*.scss")
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task('default', function(){
   gulp.watch('./src/scss/*.scss', ['sass']);
+  gulp.watch('./src/ejs/*.ejs', ['ejs']);
 });
